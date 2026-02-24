99a6e0c373e8649626f072220cb6479bf7daff21
NotRealSilver <notrealsilver@yandex.com>
2026-02-24 18:45:24 +0500

    task 2

--- program2.py
+++ program2.py
@@ -11,5 +11,14 @@ def index():
     return render_template('base.html', **param)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    params = {
+        'title': 'Тренировки',
+        'prof': prof
+    }
+    return render_template('training.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
